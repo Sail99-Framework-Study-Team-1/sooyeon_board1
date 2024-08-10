df1699257f1132e7e9a02788fd3156e12a89a921
Sooyeon <qjtmzj99@gmail.com>
2024-08-11 07:31:26 +0900

    게시글 삭제, 수정처리(오타수정)

--- src/main/java/com/example/board_sooyeon01/controller/BoardController.java
+++ src/main/java/com/example/board_sooyeon01/controller/BoardController.java
@@ -33,7 +33,7 @@ public class BoardController {
     public String boardWritePro(Board board){
 
         boardService.write(board);
-        return "";
+        return "redirect:/board/list";
     }
 
     @GetMapping("/board/list")
