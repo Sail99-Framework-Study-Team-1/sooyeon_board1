136971020ca22313d2e2d7f1381684211d1faffa
Sooyeon <qjtmzj99@gmail.com>
2024-08-10 20:17:45 +0900

    실행

---
+++ src/main/java/com/example/board_sooyeon01/BoardSooyeon01Application.java
@@ -0,0 +1,13 @@
+package com.example.board_sooyeon01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardSooyeon01Application {
+//111
+	public static void main(String[] args) {
+		SpringApplication.run(BoardSooyeon01Application.class, args);
+	}
+
+}
